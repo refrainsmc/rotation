bf481c52a97ca30e2acb64f4001a9a7344f973db
Refrains <james@blair.nz>
2022-11-11 10:48:14 +1300

    add square shape

--- main.ts
+++ main.ts
@@ -1,18 +1,72 @@
 enum Shape {
-    CIRCLE
+    CIRCLE,
+    SQUARE
+}
+
+const pauseBot = (time: number) => {
+    DFRobotMaqueenPlus.mototStop(Motors.ALL);
+    basic.pause(1000);
+}
+
+const turnRight = () => {
+    setPenState(PenState.DOWN);
+    // main turn
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 200);
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 200);
+    basic.pause(160);
+    pauseBot(1000);
+    setPenState(PenState.UP);
+    // go back a few mm
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 200);
+    basic.pause(160);
+    DFRobotMaqueenPlus.mototStop(Motors.ALL);
+}
+
+enum PenState {
+    DOWN,
+    UP
+}
+
+let penState = PenState.UP;
+DFRobotMaqueenPlus.servoRun(Servos.S3, 20);
+
+const setPenState = (state: PenState) => {
+    if (state == penState)
+        return;
+    penState = state;
+    if (state == PenState.DOWN) {
+        DFRobotMaqueenPlus.servoRun(Servos.S3, 80);
+    } else {
+        DFRobotMaqueenPlus.servoRun(Servos.S3, 20);
+    }
 }
 
 const draw = (shape: Shape) => {
     switch (shape) {
         case Shape.CIRCLE:
+            setPenState(PenState.DOWN);
             DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 200);
             basic.pause(1850);
+            DFRobotMaqueenPlus.mototStop(Motors.ALL);
+            setPenState(PenState.UP);
             break;
+        case Shape.SQUARE:
+            for (let i = 0; i < 4; i++) {
+                setPenState(PenState.DOWN);
+                DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200);
+                basic.pause(600);
+                turnRight();
+                basic.pause(1000);
+            }
+            setPenState(PenState.UP);
     }
     DFRobotMaqueenPlus.mototStop(Motors.ALL);
 }
 
-basic.forever(() => {
-    draw(Shape.CIRCLE);
-    basic.pause(1000);
-})
+try {
+    DFRobotMaqueenPlus.I2CInit();
+} catch {
+    basic.showString("Please insert into Maqueen Plus.")
+}
+
+draw(Shape.SQUARE);
