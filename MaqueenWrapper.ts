9092fafee0294c4ee93271e03f0e514e9f5db57e
Refrains <james@blair.nz>
2022-11-18 13:24:30 +1300

    updatel ololo

--- MaqueenWrapper.ts
+++ MaqueenWrapper.ts
@@ -1,7 +1,4 @@
-enum Shape {
-    CIRCLE,
-    SQUARE
-}
+const RACE_CONDITION = 500;
 
 enum Direct {
     LEFT,
@@ -13,68 +10,55 @@ enum PenState {
     UP
 }
 
+let servo: Servos;
 let penState = PenState.UP;
 
-const init = () => {
+function init(servo1: Servos) {
     DFRobotMaqueenPlus.I2CInit();
-    DFRobotMaqueenPlus.servoRun(Servos.S3, UP_ANGLE);
+    servo = servo1;
+    DFRobotMaqueenPlus.servoRun(servo, UP_ANGLE);
 }
 
-// custom runMotor function
-const runMotor = (motor: Motors, direction: Dir, speed: number, time: number) => {
+// mototRun function with time param
+function runMotor(motor: Motors, direction: Dir, speed: number, time: number) {
     DFRobotMaqueenPlus.mototRun(motor, direction, speed);
     basic.pause(time);
     DFRobotMaqueenPlus.mototStop(motor);
-    basic.pause(500);
+    // race condition
+    basic.pause(RACE_CONDITION);
 }
 
-// custom turn function
-const turn = (direction: Direct, speed: number, time: number) => {
-    if (direction === Direct.LEFT) {
-        // turn left
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, speed);
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, speed);
-    } else {
+// turn function
+function turn(direction: Direct, speed: number, time: number) {
+    if (direction == Direct.LEFT) {
         // turn left
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, speed);
         DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, speed);
+    } else {
+        // turn right
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, speed);
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, speed);
     }
+    // pause
     basic.pause(time);
     DFRobotMaqueenPlus.mototStop(Motors.ALL);
-    basic.pause(500);
+    // race condition
+    basic.pause(RACE_CONDITION);
 }
 
 // magic numbers for pen up/down
-const UP_ANGLE = 90;
-const DOWN_ANGLE = 120;
+const UP_ANGLE = 130;
+const DOWN_ANGLE = 150;
 
 // set the pen state
-const setPenState = (state: PenState) => {
+function setPenState(state: PenState) {
     if (state == penState) return; // only set angle if the pen state is different
     penState = state;
     if (state == PenState.DOWN) {
-        DFRobotMaqueenPlus.servoRun(Servos.S3, DOWN_ANGLE);
+        DFRobotMaqueenPlus.servoRun(servo, DOWN_ANGLE);
     } else {
-        DFRobotMaqueenPlus.servoRun(Servos.S3, UP_ANGLE);
+        DFRobotMaqueenPlus.servoRun(servo, UP_ANGLE);
     }
-    basic.pause(500);
-}
-
-// draw a shape
-const draw = (shape: Shape) => {
-    switch (shape) {
-        case Shape.CIRCLE:
-            setPenState(PenState.DOWN);
-            runMotor(Motors.M1, Dir.CW, 200, 1850);
-            break;
-        case Shape.SQUARE:
-            for (let i = 0; i < 4; i++) {
-                setPenState(PenState.DOWN);
-                runMotor(Motors.ALL, Dir.CW, 200, 300);
-                setPenState(PenState.UP);
-                turn(Direct.RIGHT, 200, 150);
-                runMotor(Motors.ALL, Dir.CCW, 200, 100);
-            }
-    }
-    setPenState(PenState.UP);
+    // race condition
+    basic.pause(RACE_CONDITION);
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-init();
-basic.showIcon(IconNames.Happy);
-DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW);
+// initalize
+init(Servos.S2);
 
+let size = 1;
+
+// remote control
 IR.IR_callbackUser((message) => {
+    music.playTone(Note.C, 500);
     switch (message) {
         case 16: // button 1
             draw(Shape.SQUARE);
@@ -11,24 +14,64 @@ IR.IR_callbackUser((message) => {
             draw(Shape.CIRCLE);
             break;
         case 18: // button 3
-            basic.showNumber(1);
-            turn(Direct.RIGHT, 200, 120);
-            basic.pause(1000);
-            basic.showNumber(2);
-            turn(Direct.RIGHT, 200, 140);
-            basic.pause(1000);
-            basic.showNumber(3);
-            turn(Direct.RIGHT, 200, 160);
-            basic.pause(1000);
-            basic.showNumber(4);
-            turn(Direct.RIGHT, 200, 180);
-            basic.pause(1000);
-            basic.showString("DONE");
+            draw(Shape.RECTANGLE);
+            break;
         case 8: // down arrow
             setPenState(PenState.DOWN);
             break;
         case 10: // up arrow
             setPenState(PenState.UP);
             break;
+        case 0: // power button
+            demoMode();
+            break;
     }
 });
+
+// shapes
+enum Shape {
+    CIRCLE,
+    SQUARE,
+    RECTANGLE
+}
+
+function draw(shape: Shape) {
+    setPenState(PenState.DOWN);
+    switch (shape) {
+        case Shape.CIRCLE:
+            runMotor(Motors.M1, Dir.CW, 200, 1950);
+            break;
+        case Shape.SQUARE:
+            for (let i = 0; i < 4; i++) {
+                setPenState(PenState.DOWN);
+                runMotor(Motors.ALL, Dir.CW, 200, 200);
+                setPenState(PenState.UP);
+                runMotor(Motors.ALL, Dir.CW, 200, 200);
+                turn(Direct.RIGHT, 200, 175);
+                runMotor(Motors.ALL, Dir.CCW, 200, 250);
+            }
+            break;
+        case Shape.RECTANGLE:
+            for (let i = 0; i < 4; i++) {
+                setPenState(PenState.DOWN);
+                // if i is even then 200ms, else 400ms
+                runMotor(Motors.ALL, Dir.CW, 200, i % 2 == 0 ? 200 : 400);
+                setPenState(PenState.UP);
+                runMotor(Motors.ALL, Dir.CW, 200, 200);
+                turn(Direct.RIGHT, 200, 175);
+                runMotor(Motors.ALL, Dir.CCW, 200, 250);
+            }
+    }
+    setPenState(PenState.UP);
+}
+
+function demoMode() {
+    while (true) {
+        draw(Shape.CIRCLE);
+        basic.pause(1000);
+        draw(Shape.SQUARE);
+        basic.pause(1000);
+        draw(Shape.RECTANGLE);
+        basic.pause(1000);
+    }
+}
